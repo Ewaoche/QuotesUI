48c84e079c9b115bb0a56c824778aac45c180296
techsfit-obew <techsfit@gmail.com>
2021-06-07 21:47:29 +0100

    debug heroku angular

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express')
-
+//Install express server
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/Quotes'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Quotes'));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/Quotes/' })
+app.get('/*', function(req, res) {
 
-})
+    res.sendFile(path.join(__dirname + '/dist/Quotes/index.html'));
+});
 
-app.listen(8080)
+// Start the app by listening on the default Heroku port
+app.listen(8080);
