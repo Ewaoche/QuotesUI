beedd4643a8bb1573ffc02eff1b7f0813cb4cb33
techsfit-obew <techsfit@gmail.com>
2021-06-08 12:51:22 +0100

    fixing deployment issues

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/quotes/dist'));
+// app.use(express.static(__dirname + '/quotes/dist'));
 
 // app.use(express.static(${__dirname}/front-end/dist/));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./quotes/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// app.get('*', (req, res) => {
+//     res.sendFile(`./quotes/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
+
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-
-
-
 
 // Start the app by listening on the default Heroku port
 app.listen(8080);
